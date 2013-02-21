577648b6b46df94751d86f875d0eeb271ac78ceb
Dirk Eddelbuettel <edd@debian.org>
2013-02-21 02:20:05 +0000

    more integer support

--- tests/loadFiles.R
+++ tests/loadFiles.R
@@ -10,15 +10,19 @@ fmat <- npyLoad("fmat.npy.gz")
 fmat
 stopifnot(all.equal(fmat, t(matrix(seq(0,11) * 1.1, 4, 3))))
 
-#imat <- npyLoad("imat.npy", "integer")
-#imat
-#stopifnot(all.equal(imat, t(matrix(seq(0,11), 4, 3))))
+if (npyHasIntegerSupport()) {
+    imat <- npyLoad("imat.npy", "integer")
+    imat
+    stopifnot(all.equal(imat, t(matrix(seq(0,11), 4, 3))))
+}
 
 fvec <- npyLoad("fvec.npy")
 fvec
 stopifnot(all.equal(fvec, seq(0,4) * 1.1))
 
-#ivec <- npyLoad("ivec.npy", "integer")
-#ivec
-#stopifnot(all.equal(ivec, seq(0,4)))
+if (npyHasIntegerSupport()) {
+    ivec <- npyLoad("ivec.npy", "integer")
+    ivec
+    stopifnot(all.equal(ivec, seq(0,4)))
+}
 
