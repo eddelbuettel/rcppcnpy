89e6776f2698f2b400d5d4272a81c28dd765650d
Dirk Eddelbuettel <edd@debian.org>
2013-11-29 17:10:08 -0600

    updated vignette pdf, and correction for new test

--- tests/saveAndLoad.R
+++ tests/saveAndLoad.R
@@ -3,7 +3,7 @@
 library(RcppCNPy)
 
 set.seed(42)
-M1 <- matrix(rnorm(1e6, 1e3, 1e3))
+M1 <- matrix(rnorm(1e6), 1e3, 1e3)
 
 tempmatfile <- tempfile(pattern="npymat", fileext=".npy")
 npySave(tempmatfile, M1)
