d4cc4cadfd3eaf0ca86faee666966567332b0934
Dirk Eddelbuettel <edd@debian.org>
2014-12-28 10:44:51 -0600

    directly print results from load operations, no longer test for (known !!) integer support

--- tests/loadFiles.R
+++ tests/loadFiles.R
@@ -3,26 +3,23 @@
 library(RcppCNPy)
 
 fmat <- npyLoad("fmat.npy")
-fmat
+print(fmat)
 stopifnot(all.equal(fmat, t(matrix(seq(0,11) * 1.1, 4, 3))))
 
 fmat <- npyLoad("fmat.npy.gz")
-fmat
+print(fmat)
 stopifnot(all.equal(fmat, t(matrix(seq(0,11) * 1.1, 4, 3))))
 
-if (npyHasIntegerSupport()) {
-    imat <- npyLoad("imat.npy", "integer")
-    imat
-    stopifnot(all.equal(imat, t(matrix(seq(0,11), 4, 3))))
-}
+imat <- npyLoad("imat.npy", "integer")
+print(imat)
+stopifnot(all.equal(imat, t(matrix(seq(0,11), 4, 3))))
 
 fvec <- npyLoad("fvec.npy")
-fvec
+print(fvec)
 stopifnot(all.equal(fvec, seq(0,4) * 1.1))
 
-if (npyHasIntegerSupport()) {
-    ivec <- npyLoad("ivec.npy", "integer")
-    ivec
-    stopifnot(all.equal(ivec, seq(0,4)))
-}
+ivec <- npyLoad("ivec.npy", "integer")
+print(ivec)
+stopifnot(all.equal(ivec, seq(0,4)))
+
 
