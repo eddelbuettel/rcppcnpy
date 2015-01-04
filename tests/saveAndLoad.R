2e273c0c96d1a107d6495ba7f2fb4f68b3fa5653
Wush Wu <wush978@gmail.com>
2015-01-04 11:07:55 +0800

    test if the saved object is altered or not

--- tests/saveAndLoad.R
+++ tests/saveAndLoad.R
@@ -7,13 +7,16 @@ M1 <- matrix(rnorm(1e6), 1e3, 1e3)
 
 ## double precision floating point, uncompressed
 tempmatfile <- tempfile(pattern="npymat", fileext=".npy")
+M1sd <- sd(M1)
 npySave(tempmatfile, M1)
+identical(M1sd, sd(M1))
 M2 <- npyLoad(tempmatfile)
 identical(M1, M2)
 
 ## double precision floating point, compressed
 tempmatfile <- tempfile(pattern="npymat", fileext=".npy.gz")
 npySave(tempmatfile, M1)
+identical(M1sd, sd(M1))
 M3 <- npyLoad(tempmatfile)
 identical(M1, M3)
 
@@ -22,14 +25,16 @@ identical(M1, M3)
 ## integer, uncompressed
 M4 <- matrix(as.integer(round(M1)), 1e3, 1e3)
 tempmatfile <- tempfile(pattern="intnpymat", fileext=".npy")
+M4sd <- sd(M4)
 npySave(tempmatfile, M4)
+identical(M4sd, sd(M4))
 M5 <- npyLoad(tempmatfile, "integer")
 identical(M4, M5)
 
 ## integer, compressed
 tempmatfile <- tempfile(pattern="intnpymat", fileext=".npy.gz")
 npySave(tempmatfile, M4)
-
+identical(M4sd, sd(M4))
 M6 <- npyLoad(tempmatfile, "integer")
 identical(M4, M6)
 
